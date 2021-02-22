3a65f07b9d0bb500839a53ffcca1253cb682e040
KAM513 <76922054+KAM513@users.noreply.github.com>
2021-02-21 21:32:19 -0500

    Add files via upload

---
+++ 07_Collections_Part_1/lecture-student/java/src/main/java/com/techelevator/Demo.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        Integer number = 5;
+        String s = number.toString();
+
+        num = number;
+
+        number = 7;
+
+        List <Integer> myList;
+
+        myList = new ArrayList <Integer>();
+
+
+
+    }
+}
